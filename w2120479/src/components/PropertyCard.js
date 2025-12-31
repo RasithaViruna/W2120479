75a7476a92dfcc957f56c6088923cf0669496727
RasithaViruna <rasithaviruna8@gmail.com>
2025-12-31 16:46:45 +0530

    property card style css

--- w2120479/src/components/PropertyCard.js
+++ w2120479/src/components/PropertyCard.js
@@ -1,21 +1,25 @@
 import React from "react";
+import "../styles/PropertyCard.css";
 
 function PropertyCard({ property, onAddToFavorites }) {
   return (
-    <div className="property-card" style={{ border: '1px solid #ccc', borderRadius: '10px', padding: '15px', marginBottom: '15px', backgroundColor: '#f9f9f9' }}>
+    <div className="property-card" >
 
-        <img src={property.picture} alt={property.title} style={{ width: '100%', height: '200px', objectFit: 'cover', borderRadius: '10px' }} />
+        <img src={property.picture} alt={property.title} />
+        
 
+      
         <h3>{property.location}</h3>
-        <h4 style={{ color: '#555' }}>{property.price.toLocaleString()}</h4>
+        <h4 >{property.price.toLocaleString()}</h4>
 
         <p>Type: {property.type}</p>
         <p>Bedrooms: {property.bedrooms}</p>
         <p>Description: {property.description}</p>
 
         <button 
+          className="btn favorite"
           onClick={() => onAddToFavorites(property)}
-          style={{ marginTop: '10px', padding: '10px 15px', backgroundColor: '#007bff', color: '#fff', border: 'none', borderRadius: '5px', cursor: 'pointer' }}
+
           >
             Add To Favorites
         </button>
